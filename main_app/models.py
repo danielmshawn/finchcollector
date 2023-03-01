9c72e1146ff0eab7aee0d06b7d3dbf015fbe4832
danielmshawn <danielmshawn@gmail.com>
2023-03-01 14:50:21 -0800

    Add, Edit, and Delete functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+# Import the reverse function
+
+
+from django.urls import reverse
 
 # Create your models here.
 class Finch(models.Model):
@@ -9,3 +13,6 @@ class Finch(models.Model):
 
     def __str__(self):
         return f'{self.name} ({self.id})'
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
